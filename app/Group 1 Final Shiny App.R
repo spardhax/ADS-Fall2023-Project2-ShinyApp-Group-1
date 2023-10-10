5617233f2bd555c8835016f4d989fb7ce88e9c45
dtl2129 <107059572+dtl2129@users.noreply.github.com>
2023-10-10 19:35:03 -0400

    Created another widget

--- app/Group 1 Final Shiny App.R
+++ app/Group 1 Final Shiny App.R
@@ -6,19 +6,25 @@ source("../doc/prop_vs_struc_plots_shiny.R", local = TRUE)
 
 source("../doc/median_fund_by_state_shiny.R", local = TRUE)
 
+source("../doc/descriptive_maps_property_actions.R", local = T)
+
 ui <- fluidPage(
   titlePanel("FEMA Hazard Mitigation Projects: A Breakdown Analysis"),
   tabsetPanel(
+    tabPanel("Descriptive Maps and Property Actions", ui3),
+    tabPanel("Median Funding by State/Year", ui9),
     tabPanel("Property Actions by Program", ui1),
     tabPanel("Structure Types by Program", ui2),
-    tabPanel("Median Funding by State/Year", ui9)
+    #tabPanel("Future Predictions", ui4)
   )
 )
 
 server <- function(input, output) {
+  server3(input, output)
+  server9(input, output)
   server1(input, output)
   server2(input, output)
-  server9(input, output)
+  #server4(input, output)
 }
 
 shinyApp(ui, server)
---
+++ doc/descriptive_maps_property_actions.R
@@ -0,0 +1,42 @@
+library(shiny)
+
+state_proj_count <- "../doc/figs/stateheatmap.png"
+
+state_fund_total <- "../doc/figs/statewise_mitigated_properties.jpeg"
+
+prop_actions <- "../doc/figs/prop_barplot.png"
+
+image_names <- c("Number of Projects by State", 
+                 "Total Program Funding by State", 
+                 "Number of Projects by Property Action")
+
+image_data <- data.frame(name = image_names, 
+                         path = c(state_proj_count, state_fund_total,
+                                  prop_actions), 
+                         stringsAsFactors = FALSE)
+
+ui3 <- fluidPage(
+  titlePanel("Descriptive Maps and Property Action Totals"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("imageSelect", "Select an Image:", choices = image_data$name),
+    ),
+    mainPanel(
+      imageOutput("selectedImage")
+    )
+  )
+)
+
+server3 <- function(input, output) {
+  output$selectedImage <- renderImage({
+    selected_image <- input$imageSelect
+    
+    # Get the file path for the selected image
+    selected_image_path <- image_data$path[image_data$name == selected_image]
+    
+    # Display the selected image
+    list(src = selected_image_path, width = "100%")
+  }, deleteFile = FALSE)
+}
+
+shinyApp(ui3, server3)
